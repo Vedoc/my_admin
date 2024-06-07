e88dd2a351f44321f6d57f7d90bbbb649d8445e8
arnoldnek@gmail.com <arnoldnek@gmail.com>
2024-06-07 18:26:22 +0300

    corrected seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+AdminUser.find_or_create_by(email: 'admin@mail.com') do |admin|
+    admin.password = 'password'
+    admin.password_confirmation = 'password'
+  end
+  
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+  
