73b23a6166f929face656f50b0eb19c0cb39a54c
arnoldnekemiah <arnoldnek@gmail.com>
2024-10-22 22:43:51 +0300

    test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  config.hosts.clear
+
+
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
     .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
