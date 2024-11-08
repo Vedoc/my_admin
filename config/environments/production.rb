3c10ad76433f3533d599b712f7052bb942b33454
arnoldnekemiah <arnoldnek@gmail.com>
2024-11-08 18:41:05 +0300

    tweek

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,8 +30,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = true
-  config.assets.compile = false
+  config.assets.compile = true
+  # config.assets.compile = false
   config.assets.digest = true
 
 
