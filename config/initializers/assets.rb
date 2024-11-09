a3eda47a56316c27bcd6042885911c8ff8a08913
arnoldnekemiah <arnoldnek@gmail.com>
2024-11-09 08:24:24 +0300

    fix asset styling effect

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,13 +5,12 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets')
 
 # Precompile additional assets.
-Rails.application.config.assets.precompile += %w( 
-  admin.js 
-  admin.css 
-  active_admin.scss 
-  application.css 
+Rails.application.config.assets.precompile += %w(
+  active_admin.css
+  active_admin/print.css
   active_admin.js
 )
 
