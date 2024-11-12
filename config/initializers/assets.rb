c6c6bfa432e93acb5a8c26ff28990da2e6537340
arnoldnekemiah <arnoldnek@gmail.com>
2024-11-12 11:31:06 +0300

    improved asset handling

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,11 +6,13 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'javascripts')
 
 # Precompile additional assets.
 Rails.application.config.assets.precompile += %w(
   active_admin.css
   active_admin/print.css
   active_admin.js
+  active_admin.scss
 )
 
