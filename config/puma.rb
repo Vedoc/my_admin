f9e91c286016c95978339d332aa0392b2f8de0cc
arnoldnekemiah <arnoldnek@gmail.com>
2024-11-14 20:02:36 +0300

    reverted puma file

--- config/puma.rb
+++ config/puma.rb
@@ -3,18 +3,33 @@
 # about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
 
 # Puma can serve each request in a thread from an internal thread pool.
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
 
-# Specify the port puma will listen on
+# Specifies that the worker count should equal the number of processors in production.
+if ENV["RAILS_ENV"] == "production"
+  require "concurrent-ruby"
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
+  workers worker_count if worker_count > 1
+end
+
+# Specifies the `worker_timeout` threshold that Puma will use to wait before
+# terminating a worker in development environments.
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT") { 3001 }
 
-# Specify the environment it will run in
+# Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-# Bind to all network interfaces
-bind "tcp://0.0.0.0:#{ENV.fetch("PORT") { 3001 }}"
+# Specifies the `pidfile` that Puma will use.
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-# Allow puma to be restarted by `rails restart` command
+# Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
