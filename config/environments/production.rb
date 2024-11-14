9ed04182aa918191644824f4081dd5042566665b
arnoldnekemiah <arnoldnek@gmail.com>
2024-11-14 22:06:20 +0300

    test fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.require_master_key = true
 
   # Static file serving configuration
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV.fetch("RAILS_SERVE_STATIC_FILES") { true }
 
   # Asset compilation settings
   config.assets.compile = true
