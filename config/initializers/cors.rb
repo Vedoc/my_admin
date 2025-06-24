13552614c6c9a4482067ad60213d44baf52969fa
Jaydeep Gorasiya <jaydeep.gorasiya@creolestudios.com>
2025-06-24 12:32:42 +0530

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://www.vedocapp.com'
+    origins 'https://www.vedocapp.com', 'https://api.vedocapp.com/'
 
     resource '*',
              headers: :any,
